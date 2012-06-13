20ff59574df8cc1f0e4b368c18002936a3576b8d
Matthew Pinkston <gremio42@gmail.com>
2012-06-13 16:03:11 -0400

    Changed main.cpp to main.c

---
+++ xPL_codeblocks/main.c
@@ -0,0 +1,21 @@
+// xPL Linux Hal Server
+
+#include <stdio.h>
+#include "xPLLib/xPL.h"
+
+int main(int argc, String argv[])
+{
+    int argIndex = 0;
+    xPL_ServicePtr theService = NULL;
+    xPL_MessagePtr theMessage = NULL;
+    String delimPtr;
+
+    /* Start xPL up */
+    if (!xPL_initialize(xPL_getParsedConnectionType())) {
+        fprintf(stderr, "Unable to start xPL");
+        exit(1);
+    }
+
+    printf("Hello World!");
+    return 0;
+}
