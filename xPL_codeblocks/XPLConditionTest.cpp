a91c3eed10980eb0f083b4e13e92708fc5db2d5f
Jeff Drasher <jeff.drasher@gmail.com>
2012-07-05 16:58:31 -0400

    added simple manual test file for XPLCondition

---
+++ xPL_codeblocks/XPLConditionTest.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string>
+#include "XPLCondition.h"
+
+void resultCompare(int memberCompare, int valueCompare)
+{
+	if(memberCompare != 0 && valueCompare == 0)
+	{
+		printf("Members not equal\n");
+	}
+	else if(valueCompare != 0 && memberCompare == 0)
+	{
+		printf("Values not equal\n");
+	}
+	else if(memberCompare != 0 && valueCompare != 0)
+	{
+		printf("Member and Value compares failed\n");
+	}
+	else
+	{
+		printf("Test Success!\n");	
+	}
+}
+
+int main()
+{
+	XPLValuePair pairOne, pairTwo, pairThree;
+	pairOne.member = "nameOne";
+	pairOne.value  = "valueOne";
+	pairTwo.member = "nameTwo";
+	pairTwo.value  = "valueTwo";
+
+	int memberCompare = pairOne.member.compare("name");
+	int valueCompare = pairOne.value.compare("value");
+
+	resultCompare(memberCompare, valueCompare);
+
+	return 0;		
+}
+
