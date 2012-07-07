e7019c9f988b3441507798085b9ad201181f9b15
Matthew Pinkston <gremio42@gmail.com>
2012-07-07 14:09:33 -0400

    Added XPLParser files

---
+++ xPL_codeblocks/XPLParser.h
@@ -0,0 +1,33 @@
+#ifndef XPLParser_H
+#define XPLParser_H
+
+#include "XPLMessage.h"
+
+extern "C" {
+
+    #include "xPLLib/xPL.h"
+
+}
+
+using namespace std;
+
+class XPLParser
+{
+    //PUBLIC//
+    public:
+
+	//FUNCTION PROTOTYPES
+
+    //Constructor
+    XPLParser();
+
+    //Destructor
+    ~XPLParser();
+
+    static void recvMsg(xPL_MessagePtr theMessage, xPL_ObjectPtr userValue);
+
+    static int sendMsg(XPLMessage msg);
+
+};
+
+#endif // XPLParser.h
