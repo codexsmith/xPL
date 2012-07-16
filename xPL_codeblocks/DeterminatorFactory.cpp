0e1499e64dc4726b4a55fcf7b76502a7a178df9a
Jeff Drasher <jeff.drasher@gmail.com>
2012-07-16 07:41:57 -0400

    refactoring of the factory

--- xPL_codeblocks/DeterminatorFactory.cpp
+++ xPL_codeblocks/DeterminatorFactory.cpp
@@ -1,6 +1,7 @@
 #include "DeterminatorFactory.h"
 #include "XPLAction.h"
 #include "XPLCondition.h"
+#include "XPLMessage.h"
 #include <string>
 #include <vector>
 
@@ -10,6 +11,54 @@ DeterminatorFactory::DeterminatorFactory()
 
 DeterminatorFactory::~DeterminatorFactory()
 {
+}
+
+Determinator* DeterminatorFactory::createDeterminator(string definitions[])
+{
+	Determinator* determinator;	
+	if(definitions[0].compare("-determinator") == 0)
+	{	
+		XPLCondition* condition;
+		XPLAction* action;
+	}
+	return determinator;
+}
+
+XPLCondition* DeterminatorFactory::createCondition(string definitions[])
+{
+	vector<XPLValuePair>* conditionVector = new vector<XPLValuePair>();
+	for(int i = 0; i<definitions->length; i++)
+	{
+		XPLValuePair* valuePair = new XPLValuePair(); 
+		valuePair->member = getMember(definitions[index]);
+		valuePair->value = getValue(definitions[index]);
+		conditionVector->push_back(*valuePair);
+	}
+	XPLCondition* condition = new XPLCondition(conditionVector);
+	return condition;
+}
+
+XPLAction* DeterminatorFactory::createAction(xPLMessage messages[])
+{
+	vector<XPLMessage>* actionVector = new vector<XPLMessage>();
+	for(int i = 0; i<messages.length; i++)
+	{
+		actionVector.push_back(messages[i]);
+	}
+	XPLAction* action = new XPLAction(actionVector);
+	return action;	
+}
+
+//TODO: Error checking for individuals fields
+XPLMessage* DeterminatorFactory::createsXPLMessage(string msgType, string sourceAddress, string destinationAddress, int hops, string parameters[])
+{
+	XPLMessage* message = new XPLMessage();
+	message->setMsgType(msgType);
+	string addressParameters[] = getAddressParameters(sourceAddress);
+	message->setSource(addressParameters[0], addressParameters[1], addressParameters[2]);
+	addressParameters = getAddressParameters(destinationAddress);
+	message->setSource(addressParameters[0], addressParameters[1], addressParameters[2]);
+	
 }
 
 string DeterminatorFactory::getMember(string definition)
@@ -26,34 +75,18 @@ string DeterminatorFactory::getValue(string definition)
 	return definition;	
 }
 
-Determinator* DeterminatorFactory::createDeterminator(string definitions[])
+string* DeterminatorFactory::getAddressParameters(string address)
 {
-	Determinator* determinator;	
-	if(definitions[0].compare("-determinator") == 0)
-	{	
-		XPLCondition* condition;
-		XPLAction* action;
-	}
-	return determinator;
-}
-
-XPLCondition* DeterminatorFactory::createCondition(string definitions[], int index)
-{
-	vector<XPLValuePair>* conditionVector = new vector<XPLValuePair>();
-	while(!definitions[index].compare("-action"))
+	string parameters[3];
+	int paramIndex = 0;
+	for(int i = 0; i<address.length; i++)
 	{
-		XPLValuePair* valuePair = new XPLValuePair(); 
-		valuePair->member = getMember(definitions[index]);
-		valuePair->value = getValue(definitions[index]);
-		conditionVector->push_back(*valuePair);
+		if(address.at(i) == ".")
+		{
+			parameters[0] = address.substr(0, i);
+			paramIndex++;
+			address = address.substr(i+1, address.length);	
+		}	
 	}
-	XPLCondition* condition = new XPLCondition(conditionVector);
-	return condition;
+	return parameters;
 }
-
-XPLAction* DeterminatorFactory::createAction(string definitions[], int index)
-{
-	
-}
-
-
--- xPL_codeblocks/DeterminatorFactory.h
+++ xPL_codeblocks/DeterminatorFactory.h
@@ -5,24 +5,25 @@
 #include <iostream>
 #include "XPLHal.h"
 #include "Determinator.h"
+#include "XPLMessage.h"
 
 using namespace std;
 
 //Following the format of the determinator command, the third item
 //will be the first valuePair to be defined in its condition
 
-const int startOfPairs = 2; 
-
 class DeterminatorFactory {
 	public:
 		DeterminatorFactory();
 		~DeterminatorFactory();
 		Determinator* createDeterminator(string defintions[]);
+		XPLMessage* createXPLMessage(string msgType, string destinationAddress, string sourceAddress, string schema, int hops, string parameters[]);
 
-		XPLCondition* createCondition(string definitions[], int index);
-		XPLAction* createAction(string defintions[], int index);
+		XPLCondition* createCondition(string definitions[]);
+		XPLAction* createAction(XPLMessage messages[]);
 		string getMember(string definition);
 		string getValue(string definition);
+		string* getAddressParameters(string address);
 
 	private:
 		
