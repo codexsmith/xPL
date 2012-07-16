4796d4db567cdde117dc816016f28cdbb6e8bae5
Jeff Drasher <jeff.drasher@gmail.com>
2012-07-16 17:56:24 -0400

    DeterminatorFactoryTest work

--- xPL_codeblocks/DeterminatorFactoryTest.cpp
+++ xPL_codeblocks/DeterminatorFactoryTest.cpp
@@ -1,42 +1,47 @@
 #include "DeterminatorFactory.h"
 #include <string>
 
+
+void assertSuccess(string functionName, bool result, string value)
+{
+	if(result)
+		cout << functionName +" Success!\n";
+	else
+		cout << functionName +" failed!\n Got: " << value << "\n";
+}
 void testGetMember(DeterminatorFactory* factory)
 {
 	string member = factory->getMember("member=value");	
-	if(member.compare("member") == 0)
-		cout << "testGetMember Success!\n";
-	else
-		cout << "testGetMember failed!\n Got: " << member <<"\nExpected: member";
+	bool result = (member.compare("member") == 0);
+	assertSuccess("testGetMember", result, member);
 }
 
 void testGetValue(DeterminatorFactory* factory)
 {
 	string value = factory->getValue("member=value");
-	if(value.compare("value") == 0)
-		cout << "testGetValue Success!\n";
-	else
-		cout << "testGetValue failed!\n Got: " << value << "\n";
+	bool result = (value.compare("value") == 0);
+	assertSuccess("testGetValue", result, value);
+}
+
+void testGetAddressParameters(DeterminatorFactory* factory)
+{
+	
+}
+
+void testCreateMessage(DeterminatorFactory* factory)
+{
+	string destinationAddress;
 }
 
 void testCreateCondition(DeterminatorFactory* factory)
 {
-	string definitions[] = {"-determinator", "-condition", "memberOne=valueOne", "memberTwo=valueTwo"};
-	XPLCondition* condition = factory->createCondition(definitions, 2);
-	cout << "Made it out of createCondition!\n";
-	XPLMessage message;
-	message.addMember("memberOne","valueOne");
-	message.addMember("memberTwo","valueTwo");
-	if(condition->match(&message))
-		cout << "testCreateCondition Success!\n";
-	else
-		cout << "testCreateCondition failed!\n";
+	
 }
+
 
 void testCreateAction(DeterminatorFactory* factory)
 {
-	string definitions[] = {"-action"};
-	factory->createAction(definitions, 0);
+
 }
 
 int main()
