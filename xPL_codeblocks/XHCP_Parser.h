e9aedd3f8fb011bb5be9e95b0bf9a2f5cfbe4f12
Matthew Pinkston <gremio42@gmail.com>
2012-07-22 13:49:38 -0400

    Integrated xHCP classes to project

---
+++ xPL_codeblocks/XHCP_Parser.h
@@ -0,0 +1,16 @@
+#ifndef XHCP_PARSER_H
+#define XHCP_PARSER_H
+#include "tcpserver.h"
+
+class XHCP_Parser
+{
+    public:
+        XHCP_Parser();
+        virtual ~XHCP_Parser();
+        static void recvMsg(TCPSocket *pcClientSocket, char *msg, int msgSize);
+        static void acceptMsg(TCPSocket *pcClientSocket);
+    protected:
+    private:
+};
+
+#endif // XHCP_PARSER_H
