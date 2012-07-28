9193d0187b33641fb3d6ab93b9f0274c5c678e0e
Jonathan Clyde Carmack <joncarmack@ubuntu.(none)>
2012-07-28 14:57:29 -0700

    Added map instance

--- xPL_codeblocks/XHCP_Parser.h
+++ xPL_codeblocks/XHCP_Parser.h
@@ -1,16 +1,21 @@
 #ifndef XHCP_PARSER_H
 #define XHCP_PARSER_H
 #include "tcpserver.h"
+#include "Dispatcher.h"
+#include "RuleManager.h"
 
+typedef std::string (Dispatcher::*pt2Member)(std::string);
 class XHCP_Parser
 {
     public:
         XHCP_Parser();
         virtual ~XHCP_Parser();
         static void recvMsg(TCPSocket *pcClientSocket, char *msg, int msgSize);
-        static void acceptMsg(TCPSocket *pcClientSocket);
+        static void acceptMSG(TCPSocket *pcClientSocket, char *msg, int msgSize);
     protected:
     private:
+        static std::map<std::string,pt2Member> theMap;
 };
+
 
 #endif // XHCP_PARSER_H
