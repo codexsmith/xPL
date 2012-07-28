02ca8a82c972ff71cb717f10b32bef674c788d62
Jonathan Clyde Carmack <joncarmack@ubuntu.(none)>
2012-07-28 12:04:39 -0700

    Added Dispatcher class

---
+++ xPL_codeblocks/Dispatcher.h
@@ -0,0 +1,30 @@
+#ifndef DISPATCHER_H
+#define DISPATCHER_H
+
+
+class Dispatcher{
+public:
+    
+    Dispatcher(std::string aName){
+        name = aName;
+    }
+    Dispatcher(){
+        
+    }
+    std::string addRule(std::string aString){
+        std::string theString;
+        aString += "a";
+        return aString;
+    }
+    std::string listRule(std::string aString){
+        std::string theString;
+        aString += "b";
+        return aString;
+    }
+    std::string deleteRule(std::string aString){
+        return NULL;
+    }
+private:
+    std::string name;
+};
+
