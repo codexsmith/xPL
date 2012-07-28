b537fa0b247371dff648805648c23da579939d08
Jonathan Clyde Carmack <joncarmack@ubuntu.(none)>
2012-07-28 14:53:56 -0700

    Added function pointer array (polymorpesque)

--- xPL_codeblocks/XHCP_Parser.cpp
+++ xPL_codeblocks/XHCP_Parser.cpp
@@ -3,7 +3,10 @@
 
 XHCP_Parser::XHCP_Parser()
 {
-    //ctor
+    theMap["ADDRULE"] = &Dispatcher::addRule;
+    theMap["LISTRULE"] = &Dispatcher::listRule;
+    theMap["DELETERULE"]= &Dispatcher::deleteRule;
+}
 }
 
 XHCP_Parser::~XHCP_Parser()
@@ -11,13 +14,19 @@ XHCP_Parser::~XHCP_Parser()
     //dtor
 }
 void XHCP_Parser::recvMsg(TCPSocket *pcClientSocket, char *msg, int msgSize){
-    std::string greeting( "200 GA-TECH-XPLHAL.SERVER1 Version 0.0 alpha XHCP 1.5 ready\r\n" );
-    int sendMsgSize = greeting.size();
-    int iBytesTransferred = 0;
-    pcClientSocket->SendData(greeting.c_str(), sendMsgSize);
-}
+    std::string theString(msg, msgSize);
+    std::string command;
+    while(*msg != '\n'){
+        command += *msg;
+        msg++;
+    }
+    Dispatcher aParser;
+    std::string buffer = (aParser.*theMap[theString])(theString);
+    int sendMsgSize = buffer.size();
+    pcClientSocket->SendData(buffer.c_str(), sendMsgSize);
 
-void XHCP_Parser::acceptMsg(TCPSocket *pcClientSocket){
+}
+void XHCP_Parser::acceptMSG(TCPSocket *pcClientSocket, char *msg, int msgSize){
     std::string greeting( "200 GA-TECH-XPLHAL.SERVER1 Version 0.0 alpha XHCP 1.5 ready\r\n" );
     int sendMsgSize = greeting.size();
     int iBytesTransferred = 0;
