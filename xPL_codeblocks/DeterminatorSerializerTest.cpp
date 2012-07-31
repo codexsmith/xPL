a1a9d143f96fafb6a9185b14af7adfa33adbbd27
Nicholas Smith <nix@nix-laptop.(none)>
2012-07-31 14:55:30 -0400

    serializer test

---
+++ xPL_codeblocks/DeterminatorSerializerTest.cpp
@@ -0,0 +1,35 @@
+#include "XMLpugiXPL.h"
+#include "DeterminatorSerializer.h"
+
+using namespace std;
+
+class DeterminatorSerializerTest {
+
+
+public:
+
+    void assertSuccess(string functionName, bool result, string value)
+	{
+		if(result)
+			cout << functionName +" Success!\n Got:" << value << "\n";
+		else
+			cout << functionName +" failed!\n Got: " << value << "\n";
+	}
+
+    void testReadDeterminator(DeterminatorSerializer* parser)
+    {
+        string ruleIn = parser->read();
+
+        assertSuccess("testReadDeterminator", ruleIn.compare("") != 0, ruleIn);
+
+    }
+
+    void runTests()
+    {
+
+        DeterminatorSerializer serializer = DeterminatorSerializer("determinatorSample.xml");
+        testReadDeterminator(&serializer);
+
+    }
+
+};
