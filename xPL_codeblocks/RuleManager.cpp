ffe706e5ebf188a0258fe8b94bce8b4c37415777
Matthew Pinkston <gremio42@gmail.com>
2012-07-31 17:38:13 -0400

    Cleaning up project

--- xPL_codeblocks/RuleManager.cpp
+++
@@ -1,3 +0,0 @@
-#include "RuleManager.h"
-
-
--- xPL_codeblocks/RuleMngr.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <time.h>
-#include "xPL.h"
-#include "xPL-message.h"
