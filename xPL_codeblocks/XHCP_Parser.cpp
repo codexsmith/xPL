a6410e3d43970438e9f18a361189778959957fd2
Jonathan Clyde Carmack <joncarmack@ubuntu.(none)>
2012-07-31 17:15:05 -0700

    Made changes to receive message

--- xPL_codeblocks/XHCP_Parser.cpp
+++ xPL_codeblocks/XHCP_Parser.cpp
@@ -1,9 +1,11 @@
 #include "XHCP_Parser.h"
 
 
-XHCP_Parser::XHCP_Parser()
-{
-
+XHCP_Parser::XHCP_Parser(){
+    theMap["ADDRULE"] = &Dispatcher::addRule;
+    theMap["LISTRULE"] = &Dispatcher::listRule;
+    theMap["DELETERULE"]= &Dispatcher::deleteRule;
+    theMap["CAPABILITIES"]= &Dispatcher::capCommand;
 }
 
 XHCP_Parser::~XHCP_Parser()
@@ -13,15 +15,18 @@ XHCP_Parser::~XHCP_Parser()
 void XHCP_Parser::recvMsg(TCPSocket *pcClientSocket, char *msg, int msgSize){
     std::string theString(msg, msgSize);
     std::string command;
-    while(*msg != '\n'){
-        command += *msg;
-        msg++;
-    }
+    std::string item;
+    std::vector<std::string> theList;
+    std::stringstream ss(theString);
+    while(std::getline(ss,item))
+        theList.push_back(item);
     Dispatcher aParser;
-    //std::string buffer = (aParser.*theMap[theString])(theString);
-    //int sendMsgSize = buffer.size();
-    //pcClientSocket->SendData(buffer.c_str(), sendMsgSize);
-
+    if(theList.size == 2){
+        command = list[1];
+    }
+    else command = "No Argument";
+    std::string buffer = (aParser.*theMap[theList[0]])(command);
+    pcClientSocket->SendData(greeting.c_str(), sendMsgSize);
 }
 void XHCP_Parser::acceptMsg(TCPSocket *pcClientSocket){
     std::string greeting( "200 GA-TECH-XPLHAL.SERVER1 Version 0.0 alpha XHCP 1.5 ready\r\n" );
@@ -30,9 +35,3 @@ void XHCP_Parser::acceptMsg(TCPSocket *pcClientSocket){
     pcClientSocket->SendData(greeting.c_str(), sendMsgSize);
 }
 
-void XHCP_Parser::run()
-{
-//    theMap["ADDRULE"] = &Dispatcher::addRule;
-//    theMap["LISTRULE"] = &Dispatcher::listRule;
-//    theMap["DELETERULE"]= &Dispatcher::deleteRule;
-}
