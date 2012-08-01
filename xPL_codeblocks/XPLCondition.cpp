c75b58aa54290a39659196d82c84f7d33da31a8d
Jeff Drasher <jeff.drasher@gmail.com>
2012-07-31 21:59:14 -0400

    stuff

--- xPL_codeblocks/XPLCondition.cpp
+++ xPL_codeblocks/XPLCondition.cpp
@@ -32,23 +32,23 @@ bool XPLCondition::match(XPLMessage* message)
 	XPLAddress destinationAddress = message->getDestination();
 	int hops = message->getHops();
 
-	bool msgMatch = (msgType_.compare(message->getMsgType()) == 0) || (msgType_ == NULL);
-	bool hopsMatch = (hops_ == hops) || hops_ == NULL;
-	bool sourceMatch = (sourceAddress_.vendor.compare(sourceAddress.vendor) == 0) && (sourceAddress_.device.compare(sourceAddress.device) == 0) && (sourceAddress_.instance.compare(sourceAddress.instance) == 0) || sourceAddress == NULL;
-	bool destinationMatch = (destinationAddress_.vendor.compare(destinationAddress.vendor) == 0) && (destinationAddress_.device.compare(destinationAddress.device) == 0) && (destinationAddress_.instance.compare(destinationAddress.instance) == 0) || destinationAddress == NULL;
-	
+//	bool msgMatch = (msgType_.compare(message->getMsgType()) == 0);
+//	bool hopsMatch = (hops_ == hops) || hops_ == NULL;
+//	bool sourceMatch = (sourceAddress_.vendor.compare(sourceAddress.vendor) == 0) && (sourceAddress_.device.compare(sourceAddress.device) == 0) && (sourceAddress_.instance.compare(sourceAddress.instance) == 0) || sourceAddress == NULL;
+//	bool destinationMatch = (destinationAddress_.vendor.compare(destinationAddress.vendor) == 0) && (destinationAddress_.device.compare(destinationAddress.device) == 0) && (destinationAddress_.instance.compare(destinationAddress.instance) == 0) || destinationAddress == NULL;
+
 	bool membersMatch = true;
 	for(int i = 0; i < attributes_->size(); i++)
 	{
 		XPLValuePair memberToFind = attributes_->at(i);
-		string value = message->findMember(memberToFind.member);		
+		string value = message->findMember(memberToFind.member);
 		if(!(memberToFind.value.compare(value) == 0))
 		{
 			membersMatch = false;
 			break;
 		}
 	}
-	return msgMatch && membersMatch && msgMatch && sourceMatch && destinationMatch && hopsMatch;
+	return membersMatch;
 }
 
 bool XPLCondition::equals(XPLCondition* condition)
@@ -64,7 +64,7 @@ bool XPLCondition::equals(XPLCondition* condition)
 			return false;
 		if(attribute->value.compare(compare->value) != 0)
 			return false;
-	}	
+	}
 	return true;
 }
 
